03c4e70eec8196c39af5643a5114401356e2f830
haowang <hw26@rice.edu>
2019-01-17 16:59:17 -0600

    commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 README.md
-# pipeline
+IP Fraud Detection
+
+Hao Wang
+Jan 17 2019
+
+
+Use Case:
+To read in the list of input IP information, use $ ./pipeline -train <inputfile> 
+
+To query the score of an IP address, use $ ./pipeline -query <ip_address> 
+
+Please train, i.e. input a list of valid IP login info, before using the query function
+
+
+
+Follow Up Questions
+What circumstances may lead to false positives or false negatives when using solely this score? 
+
+False positives:
+The pipeline may incorrectly predict a fraudulent IP when using solely this score when the input
+IP is just unluckily in a bad geographical neighbourhood of frauds. For a real-world example, if a traveller is
+stuck in a neighbourhood of hackers and he just logs into his online taxi account go home ASAP, this pipeline
+may lock him out of his credit card and he is not able to call a cab, which makes it a very bad day!
+
+False negatives:
+False negatives may occur may occur for the opposite reason of false positives. Just because a person
+is physically located in a good neighbourhood of faithful users with credible IP address does not mean
+he/she is not going to use fraud login credentials. For a real-world example, a guy from a hot shot
+tech company may just as well get crazy and decide to create a million fraud bots.
+
+Since we are just using latitude and longitude, the pipeline also does not take into account of 
+altitude.
+
+What challenges are there with computing distances based on latitude/longitude? 
+Since latitude and longitude are measurements in spherical coordinates, we need to apply
+the equation from spherical to Cartesian coordinates. I had to refresh myself with some 
+math from high school and confirm my calculation using some online tools such as 
+https://www.movable-type.co.uk/scripts/latlong.html
+
+Further Considerations
+Since this is an open-ended project, I need to think carefully about the use cases and the
+desired output for this pipeline.
+I also need to implement some test cases in order to cover corner cases and handle bad input.
+I also need to look through the documentation of IPinfo, as well as several other online resources
+to make the pipeline simple and intuitive.
+
+Future Improvements
+Right now the pipeline is an offline tool where the user can only input the known list
+of IPs and then input a single IP and get a single IP result. The input
+lists are stored locally in a file that will be read as input separately. 
+If we are able to store the input list in a backend, I think we can achieve faster lookup
+of closest location to the input IP as databases generally optimize such queries using techniques
+such as binary search and direct hashing. 
+
+For extensibility of the metrics, right now all input info is stored in a json file. It is 
+relatively easy to add other information provided by IPinfo, such as the name of the user, domain, 
+company name, asn etc, and it should be very interesting to see how all these factors affect the
+efficiency of the predictor pipeline.
--- pipeline.py
+++ pipeline.py
@@ -1,37 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys, ipinfo,json,os.path
-
 from collections import defaultdict
 from math import sin, cos, sqrt, atan2, radians
 
-def calculate_distance(source,dest):
+"""
+Fraud Detection Pipeline
+Hao Wang
+01/17/2019
 
-
-    # approximate radius of earth in miles
-    R = 3960.0
-
-    lat1, lon1 = source
-    lat2, lon2 = dest
-    lat1 = radians(lat1)
-    lon1 = radians(lon1)
-    lat2 = radians(lat2)
-    lon2 = radians(lon2)
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-    distance = R * c
-
-    return distance
+For instructions to use the program, 
+go to README.md
+"""
 
 
 def add_record(filename, handler):
-    
+    """
+    Given a valid input file,
+    whith each line corresponding to 
+    an authenticity and IP address, record
+    the entry into the distance_matrix table
+
+    """
 
     distance_matrix = defaultdict(lambda: defaultdict(int))
     
+    ### process the input file line
+    ### by line and record the info
     with open(filename, 'r') as f:
         for line_terminated in f:
             line = line_terminated.rstrip('\n')
@@ -45,9 +40,13 @@ def add_record(filename, handler):
 
 
 def query_result(distance_matrix, ip_address, handler):
-
-
+    """
+    Given an IP address and the distance matrix
+    of recorded logins, find the mile distance between the new 
+    login IP and the closest IP found in the input distance matrix
+    """
     
+    ### get location info from IPinfo handler
     details = handler.getDetails(ip_address)
     lat, lon = details.loc.split(',')
     location = (float(lat), float(lon))
@@ -55,7 +54,8 @@ def query_result(distance_matrix, ip_address, handler):
     score = 0
     closest_ip = ""
 
-
+    ### iterate through the matrix and 
+    ### find the closest previous record
     for each in distance_matrix:
         lat, lon = each.split(",")
         each_float = (float(lat), float(lon))
@@ -67,18 +67,50 @@ def query_result(distance_matrix, ip_address, handler):
                 score *= 2
             closest_ip = distance_matrix[each]["ip"]
     sys.stdout.write("Score of input IP address is " + str(score) + "\n")
-    sys.stdout.write("Closest of IP address is " + str(closest_ip) + "\n")
+    sys.stdout.write("Closest IP address is " + str(closest_ip) + "\n")
 
 
+def calculate_distance(source,dest):
 
+
+    """
+    Given a pair of latitude, longitude
+    coordinate pairs, compute the distance
+    in miles of two locations
+    """
+
+    ### Earth radius in miles
+    R = 3960.0
+
+    lat1, lon1 = source
+    lat2, lon2 = dest
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    ### compute distance in spherical coordinates
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
 
 def main():
 
+    ### global IPinfo ACCESS_TOKEN
     
     ACCESS_TOKEN = "035abffa9e7bda"
     handler = ipinfo.getHandler(ACCESS_TOKEN)
 
+    ### train the model
     if sys.argv[1] ==  "-train":
+        ### error handling
 
         if len(sys.argv) < 3:
             sys.stdout.write("Please provide a valid input file path")
@@ -90,15 +122,19 @@ def main():
             return 
 
         distance_matrix = add_record(filename, handler)
+        ### store results
         with open('distance_matrix.json', 'w') as outfile:  
             json.dump(distance_matrix, outfile)
     
+    ### query result
     if sys.argv[1] ==  "-query":
+
+        ### error handling
         try:
             with open('distance_matrix.json') as json_file:  
                 distance_matrix = json.load(json_file)
         except IOError:
-            sys.stdout.write("Please add record first using $ pipelin -train <filepath>")
+            sys.stdout.write("Please add record first using $ pipeline -train <filepath>")
             return 
         if len(distance_matrix) == 0:
             sys.stdout.write("No record in input file to complete query")
